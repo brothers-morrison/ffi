03d52e83a44fdd87eb58799774a8b96b6490984d
jaseg <code@jaseg.net>
2016-03-19 19:30:10 +0000

    Stage 1-8 working

--- invoke.sh
+++ invoke.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env sh
-#env PYTHONPATH=/home/jaseg/dev/toys/ffi MONO_PATH=/home/jaseg/dev/toys/ffi/mono/mcs/class/lib/net_4_x LD_LIBRARY_PATH=/usr/lib/jvm/java-8-openjdk/jre/lib/amd64/server:. java -Djava.library.path=/home/jaseg/dev/toys/ffi:/usr/lib -cp ./nativelibs4java/libraries/Mono/Mono.jar:. Stage1 test
-gdb -x gdb.init --args env PYTHONPATH=/home/jaseg/dev/toys/ffi MONO_PATH=/home/jaseg/dev/toys/ffi/mono/mcs/class/lib/net_4_x LD_LIBRARY_PATH=/usr/lib/jvm/java-8-openjdk/jre/lib/amd64/server:. java -Djava.library.path=/home/jaseg/dev/toys/ffi -cp ./nativelibs4java/libraries/Mono/Mono.jar:. -Djava.compiler=NONE Stage4 test
+#env PYTHONPATH=/home/jaseg/dev/toys/ffi MONO_PATH=/home/jaseg/dev/toys/ffi/mono/mcs/class/lib/net_4_x LD_LIBRARY_PATH=/usr/lib/jvm/java-8-openjdk/jre/lib/amd64/server:. java -Djava.library.path=/home/jaseg/dev/toys/ffi:/usr/lib -cp ./nativelibs4java/libraries/Mono/Mono.jar:. Stage1 $@
+
+DEBUG=""
+
+if [ $1 = "-d" ]; then
+    DEBUG="gdb -x gdb.init --args"
+    shift
+fi
+${DEBUG} env LD_PRELOAD=/usr/lib/jvm/java-8-openjdk/jre/lib/amd64/server/libjsig.so PYTHONPATH=/home/jaseg/dev/toys/ffi MONO_PATH=/home/jaseg/dev/toys/ffi/mono/mcs/class/lib/net_4_x LD_LIBRARY_PATH=/usr/lib/jvm/java-8-openjdk/jre/lib/amd64/server:. java -Djava.library.path=/home/jaseg/dev/toys/ffi -cp ./nativelibs4java/libraries/Mono/Mono.jar:. -Djava.compiler=NONE Stage1 $@
--- stage6.py
+++ stage6.py
@@ -3,30 +3,37 @@
 from ctypes import *
 
 def blep(foo):
-	mono = CDLL('libmonosgen-2.0.so.1')
+	try:
+		mono = CDLL('libmonosgen-2.0.so.1')
 
-	mono.mono_jit_init.restype = c_void_p
-	mono.mono_domain_assembly_open.restype = c_void_p
-	mono.mono_assembly_get_image.restype = c_void_p
-	mono.mono_class_from_name.restype = c_void_p
-	mono.mono_class_get_method_from_name.restype = c_void_p
-	mono.mono_runtime_invoke.restype = c_void_p
-	mono.mono_object_to_string = c_void_p
-	mono.mono_string_new.restype = c_void_p
-	mono.mono_string_to_utf8.restype = c_char_p
-	mono.mono_get_root_domain.restype = c_void_p
+		mono.mono_jit_init.restype = c_void_p
+		mono.mono_domain_assembly_open.restype = c_void_p
+		mono.mono_assembly_get_image.restype = c_void_p
+		mono.mono_class_from_name.restype = c_void_p
+		mono.mono_class_get_method_from_name.restype = c_void_p
+		mono.mono_runtime_invoke.restype = c_void_p
+		mono.mono_object_to_string = c_void_p
+		mono.mono_string_new.restype = c_void_p
+		mono.mono_string_to_utf8.restype = c_char_p
+		mono.mono_get_root_domain.restype = c_void_p
 
-	dom = mono.mono_get_root_domain();
-	asm = mono.mono_domain_assembly_open(dom, b"stage7.exe");
-	img = mono.mono_assembly_get_image(asm);
-	cls = mono.mono_class_from_name(img, b"", b"Stage7Runner");
-	met = mono.mono_class_get_method_from_name(cls, b"blep", 1);
+		#print('########## DOMAINS ##########')
+		#print('root: {:x}'.format(int(mono.mono_get_root_domain())&0xffffffffffffffff))
+		#print('get: {:x}'.format(int(mono.mono_domain_get())&0xffffffffffffffff))
+		#print('created: {:x}'.format(int(mono.mono_domain_create())&0xffffffffffffffff))
+		dom = c_void_p(mono.mono_get_root_domain())
+		asm = c_void_p(mono.mono_domain_assembly_open(dom, b"stage7.exe"))
+		img = c_void_p(mono.mono_assembly_get_image(asm))
+		cls = c_void_p(mono.mono_class_from_name(img, b"", b"Stage7Runner"))
+		met = c_void_p(mono.mono_class_get_method_from_name(cls, b"blep", 1))
 
-	arg = c_void_p(mono.mono_string_new(dom, c_char_p(foo.encode())))
-	mrv = mono.mono_runtime_invoke(met, None, byref(arg), None);
-	prv = mono.mono_string_to_utf8(mono.mono_object_to_string(mrv)).decode()
-
-	return prv;
+		arg = c_void_p(mono.mono_string_new(dom, c_char_p(foo.encode())))
+		mrv = mono.mono_runtime_invoke(met, None, byref(arg), None)
+		prv = mono.mono_string_to_utf8(mono.mono_object_to_string(mrv)).decode()
+		return prv
+	except Exception as e:
+		import traceback
+		traceback.print_exc()
 
 if __name__ == '__main__':
 	print(blep('test'))
