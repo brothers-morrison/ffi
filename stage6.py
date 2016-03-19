3fd98f73f45c806681d5a0f4a15aacfca8655f14
jaseg <code@jaseg.net>
2016-03-19 19:34:24 +0000

    stage6: make things less safe

--- stage6.py
+++ stage6.py
@@ -3,37 +3,29 @@
 from ctypes import *
 
 def blep(foo):
-	try:
-		mono = CDLL('libmonosgen-2.0.so.1')
+	mono = CDLL('libmonosgen-2.0.so.1')
 
-		mono.mono_jit_init.restype = c_void_p
-		mono.mono_domain_assembly_open.restype = c_void_p
-		mono.mono_assembly_get_image.restype = c_void_p
-		mono.mono_class_from_name.restype = c_void_p
-		mono.mono_class_get_method_from_name.restype = c_void_p
-		mono.mono_runtime_invoke.restype = c_void_p
-		mono.mono_object_to_string = c_void_p
-		mono.mono_string_new.restype = c_void_p
-		mono.mono_string_to_utf8.restype = c_char_p
-		mono.mono_get_root_domain.restype = c_void_p
+	mono.mono_jit_init.restype = c_void_p
+	mono.mono_domain_assembly_open.restype = c_void_p
+	mono.mono_assembly_get_image.restype = c_void_p
+	mono.mono_class_from_name.restype = c_void_p
+	mono.mono_class_get_method_from_name.restype = c_void_p
+	mono.mono_runtime_invoke.restype = c_void_p
+	mono.mono_object_to_string = c_void_p
+	mono.mono_string_new.restype = c_void_p
+	mono.mono_string_to_utf8.restype = c_char_p
+	mono.mono_get_root_domain.restype = c_void_p
 
-		#print('########## DOMAINS ##########')
-		#print('root: {:x}'.format(int(mono.mono_get_root_domain())&0xffffffffffffffff))
-		#print('get: {:x}'.format(int(mono.mono_domain_get())&0xffffffffffffffff))
-		#print('created: {:x}'.format(int(mono.mono_domain_create())&0xffffffffffffffff))
-		dom = c_void_p(mono.mono_get_root_domain())
-		asm = c_void_p(mono.mono_domain_assembly_open(dom, b"stage7.exe"))
-		img = c_void_p(mono.mono_assembly_get_image(asm))
-		cls = c_void_p(mono.mono_class_from_name(img, b"", b"Stage7Runner"))
-		met = c_void_p(mono.mono_class_get_method_from_name(cls, b"blep", 1))
+	dom = c_void_p(mono.mono_get_root_domain())
+	asm = c_void_p(mono.mono_domain_assembly_open(dom, b"stage7.exe"))
+	img = c_void_p(mono.mono_assembly_get_image(asm))
+	cls = c_void_p(mono.mono_class_from_name(img, b"", b"Stage7Runner"))
+	met = c_void_p(mono.mono_class_get_method_from_name(cls, b"blep", 1))
 
-		arg = c_void_p(mono.mono_string_new(dom, c_char_p(foo.encode())))
-		mrv = mono.mono_runtime_invoke(met, None, byref(arg), None)
-		prv = mono.mono_string_to_utf8(mono.mono_object_to_string(mrv)).decode()
-		return prv
-	except Exception as e:
-		import traceback
-		traceback.print_exc()
+	arg = c_void_p(mono.mono_string_new(dom, c_char_p(foo.encode())))
+	mrv = mono.mono_runtime_invoke(met, None, byref(arg), None)
+	prv = mono.mono_string_to_utf8(mono.mono_object_to_string(mrv)).decode()
+	return prv
 
 if __name__ == '__main__':
 	print(blep('test'))
