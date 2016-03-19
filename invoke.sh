fb3888ae9a3212ca4f5a11ac382291b853caa12e
jaseg <code@jaseg.net>
2016-03-19 17:02:37 +0000

    Better debugging

--- invoke.sh
+++ invoke.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env sh
-env PYTHONPATH=/home/jaseg/dev/toys/ffi MONO_PATH=/home/jaseg/dev/toys/ffi/mono/mcs/class/lib/net_4_x LD_LIBRARY_PATH=/usr/lib/jvm/java-8-openjdk/jre/lib/amd64/server:. java -Djava.library.path=/home/jaseg/dev/toys/ffi:/usr/lib -cp ./nativelibs4java/libraries/Mono/Mono.jar:. Stage1 test
+#env PYTHONPATH=/home/jaseg/dev/toys/ffi MONO_PATH=/home/jaseg/dev/toys/ffi/mono/mcs/class/lib/net_4_x LD_LIBRARY_PATH=/usr/lib/jvm/java-8-openjdk/jre/lib/amd64/server:. java -Djava.library.path=/home/jaseg/dev/toys/ffi:/usr/lib -cp ./nativelibs4java/libraries/Mono/Mono.jar:. Stage1 test
+gdb -x gdb.init --args env PYTHONPATH=/home/jaseg/dev/toys/ffi MONO_PATH=/home/jaseg/dev/toys/ffi/mono/mcs/class/lib/net_4_x LD_LIBRARY_PATH=/usr/lib/jvm/java-8-openjdk/jre/lib/amd64/server:. java -Djava.library.path=/home/jaseg/dev/toys/ffi -cp ./nativelibs4java/libraries/Mono/Mono.jar:. -Djava.compiler=NONE Stage4 test
